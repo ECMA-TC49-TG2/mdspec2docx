b8bc0947873e0af13fa06ab06a11a881e72cca56
Jon Skeet <skeet@pobox.com>
2019-07-01 16:37:15 +0100

    Extract Needle and FlatItem to separate files

---
+++ MarkdownConverter/Converter/FlatItem.cs
@@ -0,0 +1,12 @@
+﻿using FSharp.Markdown;
+
+namespace MarkdownConverter.Converter
+{
+    internal class FlatItem
+    {
+        public int Level;
+        public bool HasBullet;
+        public bool IsBulletOrdered;
+        public MarkdownParagraph Paragraph;
+    }
+}
