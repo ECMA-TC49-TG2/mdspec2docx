88d51825852e786faac43d316530d52f74fd4ce0
Jon Skeet <skeet@pobox.com>
2019-07-02 10:34:41 +0100

    Make ItalicUse immutable

--- MarkdownConverter/Spec/ItalicUse.cs
+++ MarkdownConverter/Spec/ItalicUse.cs
@@ -1,10 +1,10 @@
 ﻿namespace MarkdownConverter.Spec
 {
-    internal class ItalicUse
+    internal sealed class ItalicUse
     {
-        public string Literal;
-        public ItalicUseKind Kind;
-        public SourceLocation Loc;
+        public string Literal { get; }
+        public ItalicUseKind Kind { get; }
+        public SourceLocation Loc { get; }
         public enum ItalicUseKind { Production, Italic, Term };
 
         public ItalicUse(string literal, ItalicUseKind kind, SourceLocation loc)
